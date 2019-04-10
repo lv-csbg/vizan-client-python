86c2b6a931a049995e4c535227912b39573de1da
rudolfspetrovs <rudolfs.petrovs@lu.lv>
2019-04-10 15:02:08 +0100

    Initial package structure

---
+++ README.rst
@@ -0,0 +1,33 @@
+vizan-client-python
+===================
+
+.. contents:: **Table of Contents**
+    :backlinks: none
+
+Installation
+------------
+
+vizan-client-python is distributed on `PyPI <https://pypi.org>`_ as a universal
+wheel and is available on Linux/macOS and Windows and supports
+Python 2.7/3.5+ and PyPy.
+
+.. code-block:: bash
+
+    $ pip install vizan-client-python
+
+Development
+-----------
+
+To install the development version from Github:
+
+.. code-block:: bash
+
+    git clone https://github.com/lv-csbg/vizan-client-python.git
+    cd vizan-client-python
+    pip install .
+
+License
+-------
+
+vizan-client-python is distributed under the terms of `GPL v3 License <https://choosealicense.com/licenses/gpl-3.0/>`_
+
---
+++ setup.py
@@ -0,0 +1,51 @@
+from io import open
+from os import path
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'vizan_client/__init__.py'), 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.1.0'
+
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESC = f.read()
+
+REQUIRES = []
+
+setup(
+    name='vizan-client-python',
+    version=version,
+    description='A web client to VizAn REST server, global or local.',
+    long_description=LONG_DESC,
+    author='Rudolfs Petrovs',
+    author_email='rudolfs.petrovs@glu.lv',
+    maintainer='Rudolfs Petrovs',
+    maintainer_email='rudolfs.petrovs@glu.lv',
+    url='https://github.com/lv-csbg/vizan-client-python',
+    license='GPLv3',
+
+    keywords='VizAn FBA FVA',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: FSF Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(exclude=['docs', 'tests']),
+)
